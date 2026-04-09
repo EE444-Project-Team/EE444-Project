af52d5677b0eb6cbfefe11089a2fb7ade1995b02
Hannah Klebesadel <hklebesadel2@alaska.edu>
2026-04-09 14:25:44 -0800

    dded motion sensor functionality

---
+++ motionsensor.c
@@ -0,0 +1,46 @@
+#include <msp430f5438a.h>
+void motion_sensor_init(void)
+{
+
+//set up motion sensor pins-gpio
+P1SEL &= ~BIT6; //pin 1.6 i/o
+P1DIR &= ~BIT6; // pin dir output
+
+P1REN |= BIT6; //pull up/down resistor enable
+P1OUT |= BIT6; // selecting pull up
+
+P1IE |= BIT6; //interrupt enable
+P1IES |= BIT6; // hi-to-lo interrupt edge select
+
+// Testing w/ LED
+P1DIR |= P1DIR | BIT0;
+P1OUT |= BIT0;
+
+//rtc setup
+ RTCCTL1 = 0b00100000;
+ RTCSEC = time->sec;
+ RTCMIN = time->min;
+ RTCHOUR = time->hour;
+ RTCDOW = time->day_of_week;
+ RTCDAY = time->day;
+ RTCMON = 0b00000100; //setting to april
+ RTCYEARL = 0b11101010; //setting year
+ RTCYEARH = 0b00000111;
+ RTCCTL1 |= RTCRDY;
+ while (!RTCRDY);
+}
+
+void motion_ISR(void) __interrupt [PORT1_VECTOR] {
+  switch(P1IV) {
+  case 0x0E:
+  P1OUT ^= BIT0; //led testing
+  time->sec = RTCSEC;
+  time->min = RTCMIN;
+  time->hour = RTCHOUR;
+  time->day_of_week = RTCDOW;
+  time->day = RTCDAY;
+  alarm_trip(time);
+  break;
+  default: break;
+  }
+}
