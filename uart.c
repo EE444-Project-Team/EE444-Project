b924e731db0708a900e756a5b5ce1de0c6be9e7d
Ashley Schultz <akmooreschultz@alaska.edu>
2026-04-09 14:22:06 -0800

    Feat: Add code to get time from matlab in msp and matlab code

--- uart.c
+++ uart.c
@@ -1,5 +1,7 @@
 #include <msp430.h>
-#include <stdint.h>
+#include "uart.h"
+
+int size = 0;
 
 void init_uart(void) {
   
@@ -19,20 +21,43 @@ void init_uart(void) {
   TA1CCR0 = 32768;
 }
 
+
+void get_time_from_matlab(time_t *time) {
+  UCA1TXBUF = 1;
+  while(!(UCA1IFG&UCRXIFG));
+  time->day = UCA1RXBUF;
+  while(!(UCA1IFG&UCRXIFG));
+  time->day_of_week = UCA1RXBUF;
+  while(!(UCA1IFG&UCRXIFG));
+  time->hour = UCA1RXBUF;
+  while(!(UCA1IFG&UCRXIFG));
+  time->min = UCA1RXBUF;
+  while(!(UCA1IFG&UCRXIFG));
+  time->sec = UCA1RXBUF;
+}
+
+void alarm_trip(time_t *time) {
+  UCA1TXBUF = 1;
+}
+
 int x, y, z = 0;
 int i = 0;
-uint8_t byte_array[sizeof(x) * 3] = {0};
+uint8_t byte_array[100] = {0};
 void timerA_ISR(void) __interrupt[TIMER1_A0_VECTOR] {
+  time_t time;
+  get_time_from_matlab(&time);
+  alarm_trip(&time);
   x += 1;
   y += 1;
   z += 1;
   i = 0;
-  byte_array[i] = x;
+  byte_array[i] = (uint16_t) x;
   i += 2;
-  byte_array[i] = y;
+  byte_array[i] = (uint16_t) y;
   i += 2;
-  byte_array[i] = z;
+  byte_array[i] = (uint16_t) z;
   i = 0;
+  size = sizeof(x) * 3;
   UCA1IE |= UCTXIE;
   TA1CCTL0 &= ~(CCIFG); // Clear timer interrupt flag
 }
@@ -42,13 +67,12 @@ void uart_ISR(void) __interrupt[USCI_A1_VECTOR] {
     case 0:
       break;
     case 2:
-      while(!(UCA1IFG&UCTXIFG));
-      UCA1TXBUF = UCA1RXBUF;
+      
       break;
     case 4:
       UCA1TXBUF = byte_array[i];
       i++;
-      if( i >= sizeof(byte_array)) {
+      if( i >= size) {
         UCA1IE &= ~UCTXIE;
       }
       break;
--- uart.h
+++ uart.h
@@ -3,6 +3,19 @@
 // Defines interface to uart
 #ifndef UART__H
 #define UART__H
+#include <stdint.h>
+
+typedef struct {
+  uint8_t day;
+  uint8_t day_of_week;
+  uint8_t hour;
+  uint8_t min;
+  uint8_t sec;
+} time_t;
+
+void get_time_from_matlab(time_t *time);
+
+void alarm_trip(time_t *time);
 
 void init_uart(void);
 
